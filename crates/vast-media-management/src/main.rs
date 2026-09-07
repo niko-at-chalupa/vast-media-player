b80ba39274613838af44e1b33672cdf579f6d4e2
niko-at-chalupa <nikolekrescendo@protonmail.com>
2026-09-08 06:38:25 +0800

    chore(vast-media-management): fill with boilerplate

--- crates/vast-media-management/src/main.rs
+++ crates/vast-media-management/src/main.rs
@@ -1,3 +1,10 @@
-fn main() {
-    println!("Hello, world!");
+slint::include_modules!();
+
+fn main() -> anyhow::Result<()> {
+    tracing_subscriber::fmt::init();
+    
+    let ui = MainWindow::new()?;
+
+    ui.run()?;
+    Ok(())
 }
